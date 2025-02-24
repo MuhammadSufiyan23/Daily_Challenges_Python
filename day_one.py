84ed778fad1be02d5c03b543ca9ae14f944a90da
Muhammad Sufiyan <148213204+MuhammadSufiyan23@users.noreply.github.com>
2025-02-24 15:14:16 +0500

    Add files via upload

---
+++ day_one.py
@@ -0,0 +1,6 @@
+# CHALLENGE ONE COMPLETE
+
+user_input = str(input("Enter Your Name: "))
+
+print(f"Hello, Python Learners! My Name Is {user_input} 🚀 Today is the first step in our coding journey."
+ )
