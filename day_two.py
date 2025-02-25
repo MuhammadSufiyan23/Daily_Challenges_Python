93bfcdaa98deae43b6ea490f5bd5af0ccd2019f9
Muhammad Sufiyan <148213204+MuhammadSufiyan23@users.noreply.github.com>
2025-02-25 15:30:24 +0500

    Add files via upload

---
+++ day_two.py
@@ -0,0 +1,27 @@
+# DAY-2 CHALLENGE
+
+user_input = str(input("Enter Your Sentence: "))
+
+words = user_input.split()  
+total_words = len(words)  
+
+print(f"{user_input} \nTotal words: {total_words}") 
+
+# BONUS CHALLENGE
+
+user_input = input("Enter Your Sentence: ")
+
+words = user_input.split() 
+words.reverse()  
+
+print(" ".join(words))  
+
+
+
+
+
+
+
+
+
+
